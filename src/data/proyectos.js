16c94ad9737d1d2fc8b90b6ddadc068bffe9344d
agussenes <agustinsenestrari00@gmail.com>
2025-03-24 18:56:17 -0300

    cambios portafolio

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -1,75 +1,75 @@
 const proyectos = [
     {
       id: 1,
-      titulo: "UnenGroup",
-      categoria: "wordpress",
-      tecnologias: ["WordPress", "Elementor", "SEO"],
+      titulo: "Whisky & Co",
+      categoria: "MERN",
+      tecnologias: ["MongoDB", "Express", "React", "Node.js", "Diseño responsive", "SEO", "Bootstrap", "Swipper.js"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Sitio institucional desarrollado con WordPress y Elementor para el sector inmobiliario.",
-      link: "https://unengroup.com.ar"
+      descripcion: "Ecommerce desarrollado en MERN - Sigue en desarrollo",
+      link: "https://whiskyandco.com.ar/"
     },
     {
       id: 2,
-      titulo: "FOCOS",
-      categoria: "wordpress",
-      tecnologias: ["WordPress", "Diseño responsive", "SEO"],
+      titulo: "UnenGroup",
+      categoria: "Web",
+      tecnologias: ["HTML", "JavaScript", "CSS", "Bootstrap", "Diseño responsive", "SEO", "PHP", "PHPMailer"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
       descripcion: "Página para Fundación FOCOS, construida con WordPress, con énfasis en accesibilidad y contenido visual.",
-      link: "https://focos.ar"
+      link: "https://unengroup.com.ar"
     },
     {
       id: 3,
       titulo: "Rocio Fidalgo",
       categoria: "wordpress",
-      tecnologias: ["WordPress", "WooCommerce", "Diseño visual"],
+      tecnologias: ["WordPress", "WooCommerce", "Diseño visual", "Diseño responsive","SEO","Divi"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Plataforma para venta de rutinas fitness, desarrollada en WordPress + WooCommerce.",
-      link: "#"
+      descripcion: "Plataforma para venta de rutinas fitness, desarrollada en WordPress + WooCommerce. | En desarrollo",
+      link: "https://chechodev.com/"
     },
     {
       id: 4,
-      titulo: "WhiskyAndCode",
+      titulo: "Año Nuevo Chino",
       categoria: "web",
-      tecnologias: ["HTML", "CSS", "JS"],
+      tecnologias: ["HTML", "CSS", "JavaScript", "Bootstrap", "Diseño responsive", "SEO"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Landing page para comunidad de desarrollo y networking.",
-      link: "#"
+      descripcion: "Landing page para el evento de Córdoba celebra el Año Nuevo Chino | Desarrollado para la agencia de comunicacion Usina Creativa",
+      link: "https://xn--aonuevochinocba-yqb.com.ar/"
     },
     {
       id: 5,
-      titulo: "Barbería 404",
+      titulo: "Seguridad Güemes",
       categoria: "web",
-      tecnologias: ["HTML", "CSS", "JS"],
+      tecnologias: ["HTML", "JavaScript", "CSS", "Bootstrap", "Diseño responsive", "SEO", "PHP", "PHPMailer"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Sitio clásico de presentación con formulario de turnos.",
-      link: "#"
+      descripcion: "Sitio web de portafolio y solicitar trabajo para la Cooperativa de seguridad - Seguridad Güemes | Desarrollado para la agencia de comunicacion Usina Creativa",
+      link: "https://seguridadguemes.com/"
     },
     {
       id: 6,
-      titulo: "Fitness Studio",
+      titulo: "Carampagüe",
       categoria: "web",
-      tecnologias: ["HTML", "CSS", "JS"],
+      tecnologias: ["HTML", "JavaScript", "CSS", "Bootstrap", "Diseño responsive", "SEO", "PHP", "PHPMailer"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Web moderna para centro de entrenamiento personal.",
-      link: "#"
+      descripcion: " Sitio web de portafolio y solicitar trabajo para la Cooperativa de seguridad - Carampagüe | Desarrollado para la agencia de comunicacion Usina Creativa",
+      link: "https://carampague.com.ar/"
     },
     {
       id: 7,
-      titulo: "Agencia Digital Uno",
+      titulo: "Vigilancia y Seguridad Córdoba",
       categoria: "web",
-      tecnologias: ["HTML", "CSS", "JS"],
+      tecnologias: ["HTML", "CSS", "JavaScript", "Bootstrap", "Diseño responsive", "SEO"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
       descripcion: "Landing comercial con diseño limpio y animaciones.",
-      link: "#"
+      link: "https://vigilanciayseguridadcordoba.com/"
     },
     {
       id: 8,
-      titulo: "Restaurante Aventura",
+      titulo: "Amicos",
       categoria: "web",
-      tecnologias: ["HTML", "CSS", "JS"],
+      tecnologias: ["HTML", "CSS", "JS", "Bootstrap"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Página web para restaurante con galería e información de menú.",
-      link: "#"
+      descripcion: "En la agencia de Usina Creativa desarrollamos el Re Styling del Frond-end de la obra social Amicos",
+      link: "https://www.amicos.com.ar/Publico/Portal.html"
     },
     {
       id: 9,
@@ -77,35 +77,35 @@ const proyectos = [
       categoria: "web",
       tecnologias: ["HTML", "CSS", "JS"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Sitio moderno y visualmente atractivo para restaurante japonés.",
-      link: "#"
+      descripcion: "Sitio moderno y visualmente atractivo para restaurante de ramen.",
+      link: "https://inovandodes.com/"
     },
     {
       id: 10,
-      titulo: "Ecommerce Electronics",
-      categoria: "mern",
-      tecnologias: ["MongoDB", "Express", "React", "Node.js"],
+      titulo: "Focos",
+      categoria: "web",
+      tecnologias: ["HTML", "JavaScript", "CSS", "Bootstrap", "Diseño responsive", "SEO", "PHP"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Tienda online desarrollada con stack MERN para venta de tecnología.",
-      link: "#"
+      descripcion: "One Page echa para la agencia Usina Creativa",
+      link: "https://focos.ar/"
     },
     {
       id: 11,
-      titulo: "Craft Design",
-      categoria: "react",
-      tecnologias: ["React", "Styled Components", "Firebase"],
+      titulo: "Scanterra",
+      categoria: "wordpress",
+      tecnologias: ["WordPress", "WooCommerce", "Diseño visual", "Diseño responsive","SEO","WPBakery", "Jupiter thema"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Portfolio dinámico y minimalista con React y Firebase.",
+      descripcion: "Re styling del sitio completo de Scanterra para la agencia Usina Creativa",
       link: "#"
     },
     {
       id: 12,
-      titulo: "Mi Blog Dev",
-      categoria: "react",
-      tecnologias: ["React", "React Router", "Markdown"],
+      titulo: "FAARDIT",
+      categoria: "wordpress",
+      tecnologias: ["WordPress", "Diseño visual", "Diseño responsive","SEO","The Canvas"],
       imagenes: ["/assets/imagenes/faviconLogo/logoSenestraridev.png"],
-      descripcion: "Blog personal hecho en React, con soporte para artículos markdown.",
-      link: "#"
+      descripcion: "Federación Argentina de Radiología - trabajamos para Usina Creativa actualizando contenido para la web de FAARDIT y desarrollar nuevas seccionos y micro secciones",
+      link: "https://faardit.org.ar/web/"
     }
   ];
   
